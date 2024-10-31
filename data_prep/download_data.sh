33c02cc0ace3d2b344cbd9c4faa745075decbea7
ajaymin28 <ajaymin28@gmail.com>
2024-10-31 04:26:12 +0000

    download data fix

--- data_prep/download_data.sh
+++ data_prep/download_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-WORK_DIR = ~/jbhoi/
+WORK_DIR = /root/jbhoi
 
-mkdir -p $WORK_DIR/gits
-cd $WORK_DIR/gits
+mkdir -p $WORK_DIR
+cd $WORK_DIR
 
 git clone https://github.com/zhengsipeng/VRDFormer_VRD.git
 cd VRDFormer_VRD/data/vidvrd
@@ -12,20 +12,29 @@ wget https://huggingface.co/datasets/shangxd/imagenet-vidvrd/resolve/main/vidvrd
 wget https://huggingface.co/datasets/shangxd/imagenet-vidvrd/resolve/main/vidvrd-videos-part1.zip
 wget https://huggingface.co/datasets/shangxd/imagenet-vidvrd/resolve/main/vidvrd-videos-part2.zip
 
-## sudo apt-get install zip unzip
+sudo apt-get install zip unzip
+unzip vidvrd-annotations.zip
 
-unzip vidvrd-annotations.zip -d VRDFormer_VRD/data/vidvrd
-unzip vidvrd-videos-part1.zip -d VRDFormer_VRD/data/vidvrd/videos
-unzip vidvrd-videos-part2.zip -d VRDFormer_VRD/data/vidvrd/videos
+mv vidvrd-dataset/train train
+mv vidvrd-dataset/test test
+mv vidvrd-dataset/videos videos
+rm -rf vidvrd-dataset
 
-VRDDATA_VIDEOS_PATH = $WORK_DIR/gits/VRDFormer_VRD/data/vidvrd/videos
+unzip vidvrd-videos-part1.zip -d videos
+mv videos/vidvrd-videos-part1/*.mp4 videos
+rm -rf videos/vidvrd-videos-part1
+unzip vidvrd-videos-part2.zip -d videos
+mv videos/vidvrd-videos-part2/*.mp4 videos
+rm -rf videos/vidvrd-videos-part2
 
-cd $WORK_DIR/gits
+# VRDDATA_VIDEOS_PATH = $WORK_DIR/gits/VRDFormer_VRD/data/vidvrd/videos
 
-git clone https://github.com/ajaymin28/LLaVA-NeXT.git
-cd LLaVA-NeXT
+# cd $WORK_DIR/gits
 
-python data_prep/change_video_paths.py \
-    --annot_path "data_prep/video_llava_vidvrd_annotations_v7_withtime" \
-    --new_annot_save_path "data_prep/video_llava_vidvrd_annotations_v7_withtime_new_path" \
-    --new_video_path_root $VRDDATA_VIDEOS_PATH
+# git clone https://github.com/ajaymin28/LLaVA-NeXT.git
+# cd LLaVA-NeXT
+
+# python data_prep/change_video_paths.py \
+#     --annot_path "data_prep/video_llava_vidvrd_annotations_v7_withtime" \
+#     --new_annot_save_path "data_prep/video_llava_vidvrd_annotations_v7_withtime_new_path" \
+#     --new_video_path_root $VRDDATA_VIDEOS_PATH
