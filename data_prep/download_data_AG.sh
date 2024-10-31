f29488c4bdd2a381515667dd6ce0a216e97352fc
ajaymin28 <ajymin28@gmail.com>
2024-10-31 15:32:00 +0000

    AG data prep script

--- data_prep/download_data_AG.sh
+++ data_prep/download_data_AG.sh
@@ -5,5 +5,13 @@ cd $WORK_DIR
 
 git clone https://github.com/JingweiJ/ActionGenome.git
 cd ActionGenome
+mkdir -p videos
 
 wget https://ai2-public-datasets.s3-us-west-2.amazonaws.com/charades/Charades_v1_480.zip
+
+sudo apt-get install zip unzip
+
+unzip Charades_v1_480.zip -d videos
+mv videos/Charades_v1_480/*.mp4 videos
+rm -rf videos/Charades_v1_480
+mv Charades_v1_480.zip /dev/shm
